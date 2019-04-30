3366b9bc10418162a74ce091663c3126b3beedb4
lee.wp14 <lee.wp14@gmail.com>
2019-04-30 11:03:40 +0000

    Revert "move sensor blob to /vendor"
    This reverts commit ee7d2f1dbb8ec8fce3200ade5e5ffaff826477fb.

--- n7000/n7000-vendor-blobs.mk
+++ n7000/n7000-vendor-blobs.mk
@@ -17,7 +17,7 @@ LOCAL_PATH := vendor/samsung/n7000
 PRODUCT_COPY_FILES := \
     $(LOCAL_PATH)/proprietary/system/bin/gps.cer:system/bin/gps.cer \
     $(LOCAL_PATH)/proprietary/system/bin/gpsd:system/bin/gpsd \
-    $(LOCAL_PATH)/proprietary/system/lib/libakm.so:system/vendor/lib/libakm.so \
+    $(LOCAL_PATH)/proprietary/system/lib/libakm.so:system/lib/libakm.so \
     $(LOCAL_PATH)/proprietary/system/lib/libsec-ril.so:system/lib/libsec-ril.so \
     $(LOCAL_PATH)/proprietary/system/lib/libsecril-client.so:system/lib/libsecril-client.so \
     $(LOCAL_PATH)/proprietary/system/lib/hw/gps.manta.so:system/lib/hw/gps.exynos4.vendor.so
