3587e844490a4f8640e964efc3923d222174433b
lee.wp14 <lee.wp14@gmail.com>
2019-04-30 11:03:38 +0000

    Revert "n7000: move blobs to /vendor"
    This reverts commit d6567de86495c732b811cc581fa9dac4c7136322.

--- n7000/n7000-vendor-blobs.mk
+++ n7000/n7000-vendor-blobs.mk
@@ -18,9 +18,9 @@ PRODUCT_COPY_FILES := \
     $(LOCAL_PATH)/proprietary/system/bin/gps.cer:system/bin/gps.cer \
     $(LOCAL_PATH)/proprietary/system/bin/gpsd:system/bin/gpsd \
     $(LOCAL_PATH)/proprietary/system/lib/libakm.so:system/vendor/lib/libakm.so \
-    $(LOCAL_PATH)/proprietary/system/lib/libsec-ril.so:system/vendor/lib/libsec-ril.so \
-    $(LOCAL_PATH)/proprietary/system/lib/libsecril-client.so:system/vendor/lib/libsecril-client.so \
-    $(LOCAL_PATH)/proprietary/system/lib/hw/gps.manta.so:system/vendor/lib/hw/gps.exynos4.vendor.so
+    $(LOCAL_PATH)/proprietary/system/lib/libsec-ril.so:system/lib/libsec-ril.so \
+    $(LOCAL_PATH)/proprietary/system/lib/libsecril-client.so:system/lib/libsecril-client.so \
+    $(LOCAL_PATH)/proprietary/system/lib/hw/gps.manta.so:vendor/lib/hw/gps.exynos4.vendor.so
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/proprietary/system/etc/wifi/bcmdhd_apsta.bin:system/etc/wifi/bcmdhd_apsta.bin \
