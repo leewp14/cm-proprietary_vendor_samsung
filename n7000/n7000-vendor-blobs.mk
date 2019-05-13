bed2a664ea1f7a887b9e69a4176eedc019ae4b8c
lee.wp14 <lee.wp14@gmail.com>
2019-05-13 08:01:37 +0000

    n7000: bring over i9100 blobs
    Change-Id: Ie06314d1c02e00221f2813f8be474a2d9f72cd5f

--- n7000/n7000-vendor-blobs.mk
+++ n7000/n7000-vendor-blobs.mk
@@ -16,6 +16,9 @@ LOCAL_PATH := vendor/samsung/n7000
 
 PRODUCT_COPY_FILES := \
     $(LOCAL_PATH)/proprietary/system/lib/libakm.so:system/vendor/lib/libakm.so \
+    $(LOCAL_PATH)/proprietary/system/lib/libsec-ril.so:system/vendor/lib/libsec-ril.so \
+    $(LOCAL_PATH)/proprietary/system/lib/libsecril-client.so:system/vendor/lib/libsecril-client.so \
+    $(LOCAL_PATH)/proprietary/system/lib/hw/gps.exynos4.so:system/vendor/lib/hw/gps.exynos4.vendor.so
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/proprietary/system/etc/wifi/bcmdhd_apsta.bin:system/etc/wifi/bcmdhd_apsta.bin \
