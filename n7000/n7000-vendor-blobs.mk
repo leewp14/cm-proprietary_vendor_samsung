de5849ea182c93142025bcacd89696fd72b1993c
leewp14 <lee.wp14@gmail.com>
2020-02-15 00:05:14 +0800

    n7000: exclude libsecril-client.so
    We will build it instead in hardware/samsung
    
    Change-Id: I74093ad704a95bc23a8ced0acb3b5a56c393f70c

--- n7000/n7000-vendor-blobs.mk
+++ n7000/n7000-vendor-blobs.mk
@@ -17,7 +17,6 @@ LOCAL_PATH := vendor/samsung/n7000
 PRODUCT_COPY_FILES := \
     $(LOCAL_PATH)/proprietary/system/lib/libakm.so:system/vendor/lib/libakm.so \
     $(LOCAL_PATH)/proprietary/system/lib/libsec-ril.so:system/vendor/lib/libsec-ril.so \
-    $(LOCAL_PATH)/proprietary/system/lib/libsecril-client.so:system/vendor/lib/libsecril-client.so
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/proprietary/system/etc/wifi/bcmdhd_apsta.bin:system/etc/wifi/bcmdhd_apsta.bin \
