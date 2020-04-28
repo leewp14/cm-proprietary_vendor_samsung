ba67465686618a8229bc372c602a31032f3e4c61
leewp14 <lee.wp14@gmail.com>
2020-04-28 20:24:50 +0000

    galaxys2-common/n7000: remove overlay inclusion entry
    It doesn't exists. This solves Sailfish build error looking for all DEVICE_PACKAGE_OVERLAYS entries.
    Refer https://github.com/mer-hybris/hybris-boot/blob/master/Android.mk#L309
    
    Change-Id: Icb63e38ec0b577e3ac40d4421217d2c00ff727d7

--- n7000/n7000-vendor.mk
+++ n7000/n7000-vendor.mk
@@ -12,7 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Pick up overlay for features that depend on non-open-source files
-DEVICE_PACKAGE_OVERLAYS += vendor/samsung/n7000/overlay
-
 $(call inherit-product, vendor/samsung/n7000/n7000-vendor-blobs.mk)
